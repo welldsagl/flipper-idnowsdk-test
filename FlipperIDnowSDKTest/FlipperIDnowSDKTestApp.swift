62671519f99d96b5f6705aeb6ce5a3129e1cf055
Amedeo Zucchetti <amedeo.zucchetti@welld.ch>
2021-07-01 10:13:56 +0200

    Initial Commit

---
+++ FlipperIDnowSDKTest/FlipperIDnowSDKTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipperIDnowSDKTestApp.swift
+//  FlipperIDnowSDKTest
+//
+//  Created by Amedeo Zucchetti on 01.07.21.
+//
+
+import SwiftUI
+
+@main
+struct FlipperIDnowSDKTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
